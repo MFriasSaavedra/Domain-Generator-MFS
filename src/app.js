da2beba2cd84615f82e5aea7c7589d802b755284
MFriasSaavedra <martin.frias.saavedra@gmail.com>
2025-07-04 17:27:05 +0000

    arreglo for

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+
+  let pronoun = ['the', 'our', 'your', 'my'];
+  let adj = ['great', 'big', 'best', 'greatest',];
+  let noun = ['player', 'programmer', 'finder', 'creator', 'coder'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[i] + adj[a] + noun[n] + ".com")
+      }
+    }
+  }
+}
